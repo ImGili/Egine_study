d9294e7570e3036835e8c77217a8566a71b68cbf
xhguleixin123 <18017158729@163.com>
2022-01-02 23:49:03 +0800

    ✨ feat: matrix and vector

--- my_test/Quick_start/main.cpp
+++ my_test/Quick_start/main.cpp
@@ -5,13 +5,23 @@
 #include <Eigen/Core>
 #include <Eigen/Dense>
 #include <iostream>
-using Eigen::MatrixXd;
 int main()
 {
-    MatrixXd m(2,2);
+    // first Matrix
+    Eigen::MatrixXd m(2,2);
     m(0,0) = 3;
     m(1,0) = 2.5;
     m(0,1) = -1;
     m(1,1) = m(1,0) + m(0,1);
-    std::cout << m << std::endl;
+    std::cout << "m = " << std::endl << m << std::endl;
+
+    // Matrix and vector
+    Eigen::MatrixXd m_ = Eigen::MatrixXd::Random(3,3);
+    m_ = (m_ + Eigen::MatrixXd::Constant(3, 3, 1.2)) * 50;
+    std::cout << "m_ = " << std::endl << m_ << std::endl;
+
+    Eigen::VectorXd v(3);
+    v << 1, 2, 3;
+    std::cout << "v = " << std::endl << v << std::endl;
+    std::cout << "m_ * v = " << std::endl << m_*v << std::endl;
 }
